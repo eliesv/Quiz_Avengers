46ca34f9dd47a03db64ced9ac336f0c550558b4c
eliesv <38591865+eliesv@users.noreply.github.com>
2018-06-09 04:50:39 -0300

    copy e delete file funfando

--- copy_file.py
+++ copy_file.py
@@ -2,12 +2,10 @@ from shutil import copyfile
 import os
 
 def copy_file(x):
-    pathSelfie=os.path.join(os.path.expanduser("~"), "Downloads\\selfie.png")
-    pathSelfie=pathSelfie.replace("\\","/")
+    pathSelfie=os.path.join(os.path.expanduser("~"), "Downloads/selfie.png")
     source_file = pathSelfie
 
-    pathSelfieDest=os.path.join(os.path.expanduser("~"), "Downloads\\selfie\\selfie{}.png".format(x))
-    pathSelfieDest=pathSelfieDest.replace("\\","/")
+    pathSelfieDest=os.path.join(os.path.expanduser("~"), "Downloads/selfies/selfie{}.png".format(x))
     destination_file = pathSelfieDest
 
     copyfile(source_file, destination_file)
@@ -15,13 +13,11 @@ def copy_file(x):
     return None
 
 def copy_file2():
-    pathSelfie=os.path.join(os.path.expanduser("~"), "Downloads\\selfie.png")
-    pathSelfie=pathSelfie.replace("\\","/")
+    pathSelfie=os.path.join(os.path.expanduser("~"), "Downloads/selfie.png")
     source_file = pathSelfie
 
 
-    pathSelfieDest=os.path.join(os.path.expanduser("~"), "Documents\\GitHub\\Quiz_Avengers\\static\\img\\selfie.png")
-    pathSelfieDest=pathSelfieDest.replace("\\","/")
+    pathSelfieDest=os.path.join(os.path.expanduser("~"), "Documents/GitHub/Quiz_Avengers/static/img/selfie.png")
     destination_file = pathSelfieDest
 
     copyfile(source_file, destination_file)
