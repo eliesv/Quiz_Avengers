a6c19ea45914fc3d0989dae117b1bf5caf533858
freddy-d <38051127+freddy-d@users.noreply.github.com>
2018-06-09 10:17:59 -0300

    fudeu juntar tudo

--- Photos.py
+++ Photos.py
@@ -5,58 +5,64 @@
 import cv2
 import os
 import json
+
 #def overlay():
+
 with open('variaveis.json','r') as variaveis:
     dicjson = json.loads(variaveis.read())
     vingador = dicjson["vencedor"]
 
-face_cascade = cv2.CascadeClassifier('C:/Users/Freddy Dratwa/Downloads/opencv/sources/data/haarcascades/haarcascade_frontalface_default.xml')
-f_image = cv2.imread('C:/Users/Freddy Dratwa/Documents/GitHub/Quiz_Avengers/photos\{}.png'.format(vingador), -1)
+    cascadePath = os.path.join(os.path.expanduser("~"), "Downloads/opencv/sources/data/haarcascades/haarcascade_frontalface_default.xml")
+    vingadorPath = os.path.join(os.path.expanduser("~"), 'Documents/GitHub/Quiz_Avengers/photos\{}.png'.format(vingador)
 
-f_mask = f_image[:, :, 3]
-b_mask = cv2.bitwise_not(f_mask)
-f_image = f_image[:, :, 0:3]
+    face_cascade = cv2.CascadeClassifier(cascadePath)
+    f_image = cv2.imread(vingadorPath, -1)
 
-f_size = 570
-f_ratio = float(f_size)
+    f_mask = f_image[:, :, 3]
+    b_mask = cv2.bitwise_not(f_mask)
+    f_image = f_image[:, :, 0:3]
 
-b_size = 2000
+    f_size = 570
+    f_ratio = float(f_size)
 
-padding_top = ((b_size - f_size) / 3) * 2
-padding_bottom = b_size - padding_top
-padding_left = (b_size - f_size) / 2
-padding_right = (b_size - f_size) / 2
+    b_size = 2000
 
-
-
-cv_image = cv2.imread('C:/Users/Freddy Dratwa/Documents/GitHub/Quiz_Avengers/static/img/selfie.png')
-
-faces = face_cascade.detectMultiScale(
-                cv_image,
-                scaleFactor=1.1,
-                minNeighbors=3,
-                minSize=(30, 30),
-                flags=cv2.CASCADE_SCALE_IMAGE)
-for (x1, y1, w, h) in faces:
-
-    x2 = x1 + w
-    y2 = y1 + h
-    face_roi = cv_image[y1:y2, x1:x2]
-
-    ratio = f_ratio / face_roi.shape[1]
-    dimension = (f_size, int(face_roi.shape[0] * ratio))
-    face = cv2.resize(face_roi, dimension, interpolation = cv2.INTER_AREA)
-
-    b_image = cv2.copyMakeBorder(face, int(padding_top), int(padding_bottom), int(padding_left), int(padding_right), cv2.BORDER_CONSTANT)
-
-    b_src = b_image[0:b_size, 0:b_size]
-    roi_bg = cv2.bitwise_and(b_src,  b_src, mask=b_mask)
-    roi_fg = cv2.bitwise_and(f_image, f_image, mask=f_mask)
-    dst = cv2.add(roi_bg, roi_fg)
+    padding_top = ((b_size - f_size) / 3) * 2
+    padding_bottom = b_size - padding_top
+    padding_left = (b_size - f_size) / 2
+    padding_right = (b_size - f_size) / 2
 
 
-    cv2.imwrite("C:/Users/Freddy Dratwa/Documents/GitHub/Quiz_Avengers/static/img/vingador.png", dst)
-    cv2.waitKey()
-    cv2.destroyAllWindows()
-    break
-    #return None
+
+    cv_image = cv2.imread('/img/selfie.png')
+
+    faces = face_cascade.detectMultiScale(
+                    cv_image,
+                    scaleFactor=1.1,
+                    minNeighbors=3,
+                    minSize=(30, 30),
+                    flags=cv2.CASCADE_SCALE_IMAGE)
+    for (x1, y1, w, h) in faces:
+
+        x2 = x1 + w
+        y2 = y1 + h
+        face_roi = cv_image[y1:y2, x1:x2]
+
+        ratio = f_ratio / face_roi.shape[1]
+        dimension = (f_size, int(face_roi.shape[0] * ratio))
+        face = cv2.resize(face_roi, dimension, interpolation = cv2.INTER_AREA)
+
+        b_image = cv2.copyMakeBorder(face, int(padding_top), int(padding_bottom), int(padding_left), int(padding_right), cv2.BORDER_CONSTANT)
+
+        b_src = b_image[0:b_size, 0:b_size]
+        roi_bg = cv2.bitwise_and(b_src,  b_src, mask=b_mask)
+        roi_fg = cv2.bitwise_and(f_image, f_image, mask=f_mask)
+        dst = cv2.add(roi_bg, roi_fg)
+
+
+        cv2.imwrite("/img/vingador.png", dst)
+        cv2.waitKey()
+        cv2.destroyAllWindows()
+        break
+
+        #return None
